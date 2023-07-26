8a5cb8ce0e5000b7aa6a4c913733979de5e9acaf
saidulixlam <saidulixlam@gmail.com>
2023-07-26 15:35:14 +0530

    Delete and edit feature improved

--- index.js
+++ index.js
@@ -17,17 +17,18 @@ function addUser(e) {
     }
 
     if (submit.innerText === 'Update') {
-        axios.put(`https://crudcrud.com/api/d3c0440b2d434c6d8766be07f0273402/form/${userId}`, user)
+        //console.log(userId);
+        axios.put(`https://crudcrud.com/api/45659d83032a4360bf4078106b37ffa7/form/${user._id}`, user)
             .then((res) => {
                 console.log(res);
                 removeItem(userId);
             }).catch((err) => {
                 console.log(err);
             })
-        //
+
 
     } else {
-        axios.post(`https://crudcrud.com/api/d3c0440b2d434c6d8766be07f0273402/form`, user)
+        axios.post(`https://crudcrud.com/api/45659d83032a4360bf4078106b37ffa7/form`, user)
             .then((res) => {
                 console.log(res);
             }).catch((err) => {
@@ -35,14 +36,16 @@ function addUser(e) {
             })
     }
     displayUser(user);
+    //after user details saved we can access id by (user._id)
+    //console.log(user._id);
     clearuserInput();
-    submit.value = "Submit"
     submit.innerText = "Submit"
+    submit.value = "Submit"
     submit.style.background="grey"
 }
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    axios.get("https://crudcrud.com/api/d3c0440b2d434c6d8766be07f0273402/form")
+    axios.get("https://crudcrud.com/api/45659d83032a4360bf4078106b37ffa7/form")
         .then((res) => {
             for (var i = 0; i < res.data.length; i++) {
                 displayUser(res.data[i]);
@@ -53,6 +56,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
 })
 
 function displayUser(user) {
+
     let li = `<li id=${user._id}> ${user.namee} - ${user.email} - ${user.number}
      <button onclick=deleteUser('${user._id}')>Delete User</button>
      <button onclick=editUser('${user._id}','${user.namee}','${user.email}','${user.number}')>Edit User</button>
@@ -63,28 +67,30 @@ function displayUser(user) {
 
 function removeItem(userId) {
     const elm = document.getElementById(userId);
+    //console.log(userId)
     list.removeChild(elm);
 }
 function deleteUser(id) {
-    axios.delete(`https://crudcrud.com/api/d3c0440b2d434c6d8766be07f0273402/form/${id}`)
+    removeItem(id);
+    axios.delete(`https://crudcrud.com/api/45659d83032a4360bf4078106b37ffa7/form/${id}`)
         .then((res) => {
             console.log('delete');
         }).catch((err) => {
             console.log(err);
         })
-    removeItem(id);
+    
 }
 function editUser(id, name, email, number) {
     document.getElementById('name').value = name;
     document.getElementById('email').value = email;
     document.getElementById('number').value = number;
     userId = id;
-
     submit.value = "Update";
     submit.style.background = "green";
+    removeItem(id);
 }
 function clearuserInput() {
-    document.getElementById('name').value = ' ';
-    document.getElementById('email').value = ' ';
-    document.getElementById('number').value = ' ';
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('number').value = '';
 }
